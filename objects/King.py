c9dbc173a902092f74f59bafcbe6fac250f05e3b
Yannic-L <y.luetkenhaus@group24.de>
2023-11-22 16:59:42 +0100

    Merge

--- objects/King.py
+++ objects/King.py
@@ -2,6 +2,6 @@ from objects.GamePiece import GamePiece
 
 
 class King(GamePiece):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,wiking_nummber):
+        super().__init__(wiking_nummber)
         print('but wait im also the king')
