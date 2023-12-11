06536154875b2e253ea2a83d937871ee747bae67
Yannic-L <y.luetkenhaus@group24.de>
2023-12-11 09:58:22 +0100

    test

--- objects/AimAssist.py
+++ objects/AimAssist.py
@@ -1,5 +1,3 @@
-import os
-
 import pygame as pg
 
 
@@ -8,26 +6,29 @@ class AimAssist:
         self.up_down_moving = False
         self.side_side_moving = False
         self.screen = pg.display.get_surface()
-        self.angel=0
-        self.throwPower = self.screen.get_width()/4
+        self.angel = 0
+        self.throwPower = self.screen.get_width() / 4
         self.height_direction = False
         self.width_direction = False
         self.start_side = True
-        self.vector = pg.Vector2()
 
     def reset(self):
-        self.throwPower =self.screen.get_width()/4
+        self.throwPower = self.screen.get_width() / 4
 
         self.angel=0
         self.up_down_moving = False
         self.side_side_moving = False
+
     def get_angel(self):
-        return  self.angel
+        return self.angel
 
     def get_throwPower(self):
         return self.throwPower
+
     def get_vector(self):
-        return self.vector
+        vector=pg.Vector2()
+        vector.from_polar((self.get_throwPower(), self.get_angel()))
+        return vector
 
     def is_up_down_moving(self):
         return self.up_down_moving
@@ -48,28 +49,24 @@ class AimAssist:
         self.side_side_moving = False
 
     def update(self, game_state):
-        if game_state == 'aim_assist':
+        if self.up_down_moving:
             self.move_angel()
-        if game_state == 'trow_power':
+        if self.side_side_moving:
             self.move_throwPower()
 
-        self.vector.from_polar((self.get_throwPower(), self.get_angel()))
-
     def move_angel(self):
-        if self.up_down_moving:
             if not self.height_direction:
-                if self.angel <70:
+                if self.angel < 70:
                     self.angel += 2
                 else:
                     self.height_direction = True
             elif self.height_direction:
-                if self.angel >-70:
+                if self.angel > -70:
                     self.angel -= 2
                 else:
                     self.height_direction = False
 
     def move_throwPower(self):
-        if self.side_side_moving:
             if not self.width_direction:
                 if self.throwPower < self.screen.get_width():
                     self.throwPower += 10
--- objects/GamePiece.py
+++ objects/GamePiece.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import objects.Game as Game
+import objects.Game as game
 import os
 
 
@@ -7,22 +7,27 @@ class GamePiece(pg.sprite.Sprite):
 
     def __init__(self, pos_x, pos_y):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = Game.load_image("game_piece.png")
+        self.image, self.rect = game.load_image("game_piece.png")
         self.screen = pg.display.get_surface()
         self.area = self.screen.get_rect()
         self.rect.topleft = pos_x - self.rect.width / 2, pos_y - self.rect.height / 2
+        self.rethrown= False
+        self.rethrowActiveGamePiece= False
 
-    def update(self, aimAssistVector, game_state):
-        # self.rect.topleft = pos_x - self.rect.width/2, pos_y - self.rect.height/2
-        # if game_state == 'hit':
-        if self.check_kolision(aimAssistVector) and game_state == 'hit':
+    def update(self, aimAssistVector, game_state,game):
+        if self.check_collision(aimAssistVector) and game_state == 'hit':
             self.image = pg.transform.grayscale(self.image, self.image)
+            game.game_state='rethrow'
+            game.aim_assist.reset()
+            self.rethrown=True
+            self.rethrowActiveGamePiece=True
+        if self.rethrowActiveGamePiece:
+            self.rect
         return
 
-    def check_kolision(self, aimAssistVector):
+    def check_collision(self, aimAssistVector):
 
-        if (aimAssistVector.distance_to(self.rect.center) <= 50):
-            print(aimAssistVector.distance_to(self.rect.center))
+        if (aimAssistVector.distance_to(self.rect.center) <= 60):
             return True
         else:
             return False
